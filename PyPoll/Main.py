751236b656a3cfab7f38403c3ca7718f96b37286
Cindy Reznikov <36276434+cindyreznikov@users.noreply.github.com>
2020-02-22 02:36:38 -0600

    pypoll homework

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -4,13 +4,15 @@ import operator
 
 # identify where the files are stored
 
-election_data_csv_path = os.path.join('..', 'Resources', 'election_data.csv')
-output_path = os.path.join('..','Resources', 'election_results.txt')
+election_data_csv_path = os.path.join('Resources', 'election_data_smaller.csv')
+output_path = os.path.join('Resources', 'election_results.txt')
 
-candidate_dictionary {}
+candidate_dictionary = {}
 
 total_voters = 0
 total_candidate_votes = 0
+index = 0
+first_candidate = 'True'
 
 with open(election_data_csv_path) as csvfile:
     election_data_csv = csv.reader(csvfile, delimiter=",")
@@ -18,30 +20,41 @@ with open(election_data_csv_path) as csvfile:
 
     #  sort by the 3rd item which is candidate and move the 1st candidate to the previous_candidate variable
     sorted_input = sorted(election_data_csv,key=operator.itemgetter(2))
-    previous_candidate = str(row[2])
+        
+    for row in sorted_input:
+        candidate_in = str(row[2])
+        total_voters = total_voters + 1
 
-     for row in sorted_input:
-         candidate_in = str(row[2])
-         total_voters = total_voters + 1
-                
+        if first_candidate:
+            previous_candidate = str(row[2])
+            first_candidate = False
+            
         if candidate_in != previous_candidate:
-            candidate_dictionary(candidate_in: total_candidate_votes)
+            index = index + 1
+            candidate_dictionary[index] = previous_candidate
+            candidate_dictionary[index] = total_candidate_votes
             previous_candidate = candidate_in 
             total_candidate_votes = 1
         else:
             total_candidate_votes = total_candidate_votes + 1
+
+# move the last candidate to the dictionary
+            
+index = index + 1
+candidate_dictionary[index] = previous_candidate
+candidate_dictionary[index] = total_candidate_votes
 
 print("Election Results") 
 print("----------------------------")
 print(f"Total Votes: {str(total_voters)}")
 print("-----------------------------")
 
-For key, value in sorted(candidate_dictionary.items(), key=lambda item: item[1]):
-   vote_percent = value/total_voters
-   print("key, + vote_percent, + ( value + ) )  
+for key, value in sorted(candidate_dictionary.items(), reverse=True, key=lambda item: item[1]):
+   vote_percent = value/total_voters * 100
+   print(f"{str(key)} {(round(vote_percent,3))}% ({str(value)})")  
 
 print("-----------------------------")
-print(f"Winner: value[1]")
+print(f"Winner: {str(key)}")
 print("-----------------------------")
 
 file = open(output_path, 'w') 
@@ -49,14 +62,16 @@ file.write("Election Results \n")
 file.write("----------------------------\n")
 file.write(f"Total Votes: {str(total_voters)}\n")
 
-print("-----------------------------")
+file.write("-----------------------------\n")
 
-For key, value in sorted(candidate_dictionary.items(), key=lambda item: item[1]):
-   vote_percent = value/total_voters
-   print("key, + vote_percent, + ( value + ) )  
+#For key, value in sorted(candidate_dictionary.items(), reverse=True, key=lambda item: item[1]):
 
-file.write("-----------------------------")
-file.write(f"Winner: value[1]\n")
-file.write("-----------------------------")
+for key, value in candidate_dictionary.items():
+   vote_percent = value/total_voters * 100
+   file.write(f"key {(round(vote_percent,3))}% ({str(value)}) \n")  
+
+file.write("-----------------------------\n")
+file.write(f"Winner: {str(key)} \n")
+file.write("-----------------------------\n")
 
 file.close()
