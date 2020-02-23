3b06393c2dcd39f30bc8a4280aa345f17862ea6e
Cindy Reznikov <36276434+cindyreznikov@users.noreply.github.com>
2020-02-22 23:46:39 -0600

    python homework changes

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -8,41 +8,51 @@ with open(budget_data_csv_path) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
 
-    total_months = 0
-    net_total_amount = 0
-    greatest_increase = 0
-    greatest_decrease = 0
+    date_list = []
+    profit_loss_list = []
+    profit_loss_change_list = []
+
+# load the input data into 2 lists - 1 for profit/loss, 1 for date
 
     for row in csv_reader:
-        month_year = str(row[0])
-        profit_loss = int(row[1])
-        
-        total_months = total_months + 1
-        net_total_amount = net_total_amount + profit_loss
-        if profit_loss > 0:
-             if greatest_increase < profit_loss:
-                   greatest_increase = profit_loss
-                   greatest_increase_month = month_year
-        elif greatest_decrease > profit_loss:
-                   greatest_decrease = profit_loss
-                   greatest_decrease_month = month_year
+        profit_loss_list.append(float(row[1]))
+        date_list.append(row[0])
 
-avg_change = net_total_amount/total_months
+total_profit_loss = sum(profit_loss_list)
+# Load the profit_loss_change list with differences between 2 dates
+
+for i in range(1,len(profit_loss_list)):
+    profit_loss_change_list.append(profit_loss_list[i] - profit_loss_list[i-1])
+    avg_profit_loss_change = sum(profit_loss_change_list)/len(profit_loss_change_list) 
+
+    min_profit_loss_change = min(profit_loss_change_list)
+    max_profit_loss_change = max(profit_loss_change_list)
+
+    max_profit_loss_change_date = str(date_list[profit_loss_change_list.index(max(profit_loss_change_list))+1])
+    min_profit_loss_change_date = str(date_list[profit_loss_change_list.index(min(profit_loss_change_list))+1])
+
+
+# print to the screen
 
 print("Financial Analysis") 
 print("----------------------------")
-print(f"Total Months: {str(total_months)}")
-print(f"Total: ${str(net_total_amount)}")
-print(f"Average Change: $ {(round(avg_change,2))}")
-print(f"Greatest Increase in Profits: {str(greatest_increase_month)}  (${str(greatest_increase)})")      
-print(f"Greatest Decrease in Profits: {str(greatest_decrease_month)}  (${str(greatest_decrease)})")
+print("Total Months: ", len(date_list))
+print(f"Total: ${int(total_profit_loss)}")
+
+print("Average Change: $", (round(avg_profit_loss_change,2)))
+
+print(f"Greatest Increase in Profits: {str(max_profit_loss_change_date)}  (${int(max_profit_loss_change)})")      
+print(f"Greatest Decrease in Profits: {str(min_profit_loss_change_date)}  (${int(min_profit_loss_change)})")
+
+# write to a csv file
 
 file = open(output_path, 'w') 
 file.write("Financial Analysis \n") 
 file.write("----------------------------\n")
-file.write(f"Total Months: {str(total_months)}\n")
-file.write(f"Total: ${str(net_total_amount)}\n")
-file.write(f"Average Change: $ {(round(avg_change,2))} \n")
-file.write(f"Greatest Increase in Profits: {str(greatest_increase_month)} (${str(greatest_increase)})\n")      
-file.write(f"Greatest Decrease in Profits: {str(greatest_decrease_month)} (${str(greatest_decrease)})\n")
+file.write(f"Total Months: {str(len(date_list))}\n")
+file.write(f"Total: ${int(total_profit_loss)}\n")
+
+file.write(f"Average Change: $ {(round(avg_profit_loss_change,2))} \n")
+file.write(f"Greatest Increase in Profits: {str(max_profit_loss_change_date)} (${int(max_profit_loss_change)})\n")      
+file.write(f"Greatest Decrease in Profits: {str(min_profit_loss_change_date)} (${int(min_profit_loss_change)})\n")
 file.close()
